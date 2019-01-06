b19b8827da40c8b9ea8eb4111b9e5a981418a6ce
Suman Nandi <Suman@sumans-macbook-pro.local>
2019-01-06 14:53:21 +0530

    master branch created

---
+++ Countries/Countries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by Suman Nandi on 1/6/19.
+//  Copyright © 2019 Earthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
